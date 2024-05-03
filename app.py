ce86a5682614ec8c99b88980be33a1be6cd5d2cb
Adriana <adriigarr@gmail.com>
2024-05-03 15:06:18 +0200

    3/mayo

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.secret_key = 'your_super_secret_key'  # Change this to a random secret key
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''  # Your MySQL password (if set)
-app.config['MYSQL_DB'] = 'testing'  # Your database name
+app.config['MYSQL_DB'] = 'testingQuizs'  # Your database name
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 app.config['MYSQL_UNIX_SOCKET'] = '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock'
 
@@ -37,13 +37,14 @@ def login():
         user = cursor.fetchone()
         if user:
             session['loggedin'] = True
-            session['userid'] = user['userid']
+            session['userid'] = user['userID']
             session['name'] = user['name']
             message = 'Logged in successfully!'
             return render_template('user.html', message=message)
         else:
             message = 'Incorrect username/password!'
     return render_template('login.html', message=message)
+
 
 @app.route('/logout')
 def logout():
@@ -52,13 +53,19 @@ def logout():
     session.pop('name', None)
     return redirect(url_for('login'))
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     message = ''
-    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form:
+    if request.method == 'POST' and 'name' in request.form and 'surname1' in request.form and 'surname2' in request.form and 'gender' in request.form and 'dob' in request.form and 'email' in request.form and 'password' in request.form:
         name = request.form['name']
-        password = request.form['password']
+        surname1 = request.form['surname1']
+        surname2 = request.form['surname2']
+        gender = request.form['gender']
+        dateBirth = request.form['dob']
         email = request.form['email']
+        password = request.form['password']
+
         cursor = mysql.cursor()
         cursor.execute('SELECT * FROM user WHERE email = %s', (email,))
         account = cursor.fetchone()
@@ -66,13 +73,15 @@ def register():
             message = 'Account already exists!'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             message = 'Invalid email address!'
-        elif not name or not password or not email:
+        elif not (name and surname1 and surname2 and gender and dateBirth and email and password):
             message = 'Please fill out the form!'
         else:
-            cursor.execute('INSERT INTO user (name, email, password) VALUES (%s, %s, %s)', (name, email, password))
+            cursor.execute('INSERT INTO user (name, surname1, surname2, gender, dateBirth, email, password) VALUES (%s, %s, %s, %s, %s, %s, %s)',
+                           (name, surname1, surname2, gender, dateBirth, email, password))
             mysql.commit()
             message = 'You have successfully registered!'
     return render_template('register.html', message=message)
+
 
 # Define the route for pregunta1
 @app.route('/pregunta1')
@@ -81,26 +90,27 @@ def pregunta1():
     return render_template('pregunta1.html')
 
 
-# Define the route for pregunta1
+# Define the route for pregunta2
 @app.route('/pregunta2')
 def pregunta2():
     # Your logic for the quiz page
     return render_template('pregunta2.html')
 
 
-# Define the route for pregunta1
+# Define the route for pregunta3
 @app.route('/pregunta3')
 def pregunta3():
     # Your logic for the quiz page
     return render_template('pregunta3.html')
 
-# Define the route for pregunta1
+
+# Define the route for resultados
 @app.route('/resultados')
 def resultados():
     # Your logic for the quiz page
     return render_template('resultados.html')
 
-# Define the route for pregunta1
+# Define the route for dashboard
 @app.route('/user')
 def user():
     # Your logic for the quiz page
